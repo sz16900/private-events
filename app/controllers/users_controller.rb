2c576bc9d634bc63be2105694db2e4306a27e091
Luis Saavedra <nriqu322@gmail.com>
2020-04-24 10:44:44 -0600

    add signup page and layout navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,25 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
 
     def new
-        
+      @user = User.new
     end
 
     def create
-        @user = User.new(user_params)
+      @user = User.new(user_params)
+
+      respond_to do |format|
+        if @user.save
+          format.html { redirect_to @user, notice: 'User was successfully created.' }
+          format.json { render :show, status: :created, location: @user }
+        else
+          format.html { render :new }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
     def show
-
+      @user = set_user
     end
 
     private
@@ -17,12 +27,12 @@ class UserController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+
     end
   
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:name, :email)
     end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: [:new, :create, :show] 
+
+  root 'events#index'
 end
