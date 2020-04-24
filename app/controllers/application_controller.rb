64b236e97c067af0b708f1b34259ad9402b480db
Luis Saavedra <nriqu322@gmail.com>
2020-04-24 12:18:38 -0600

    add session log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        @user = User.find_by(id: session[:user_id]) if session[:user_id]
-      end
-    
-    def signed_in?
-    !current_user.nil?
-    end
+  include SessionsHelper
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def current_user
+    @user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  
+  def signed_in?
+    !current_user.nil?
+  end
 end
