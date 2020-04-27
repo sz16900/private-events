0b98c4de911b32a02d867f8bce7e2e912ebfb9f2
sz16900 <sz16900@my.bristol.ac.uk>
2020-04-27 11:50:29 -0500

    :recycle: refactor
    refactoring event model from methods to scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,12 +3,20 @@ class Event < ApplicationRecord
     has_many :invitations, foreign_key: :attended_event_id
     has_many :attendees, through: :invitations
 
-    def self.past
-        Event.where('date < date(\'now\')')
-    end
 
-    def self.upcoming
-        Event.where('date > date(\'now\')')
-    end
+    # With methods:
+
+    # def self.past
+    #     Event.where('date < date(\'now\')')
+    # end
+
+    # def self.upcoming
+    #     Event.where('date > date(\'now\')')
+    # end
+
+    # With scopes:
+
+    scope :past, -> {where("date < date('now')")}
+    scope :upcoming, -> {where("date > date('now')")}
 
 end
