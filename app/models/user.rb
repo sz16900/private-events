1271d6a8a9e3fc02b00bee4106d1cfc20ac8ae88
sz16900 <sz16900@my.bristol.ac.uk>
2020-04-27 15:45:53 -0500

    :white_check_mark: user model test
    invitation is populated with user and event info

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     has_many :attended_events, through: :invitations
 
     validates :name, :email, presence: true
+    validates :email, uniqueness: true
 
     def upcoming_events
         attended_events.where('date > date(\'now\')')
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,31 +3,26 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.create(name: 'Example User', email: 'user@example.com')
-    @event = Event.create(title: 'Awesome party ya\'ll',
+    @user = User.new(name: 'Example User', email: 'user@example.com')
+    @event = Event.new(title: 'Awesome party ya\'ll',
                           description: 'Gonna be lit',
                           date: '04-20-2020',
                           place: 'Kenya',
                           creator_id: @user.id)
-    @invitation = Invitation.create(attendee_id: @user.id , attended_event_id: @event.id)
   end
 
   test "association creator_id exist" do
+    @user.save
+    @event.creator_id = @user.id
+    @event.save
     assert_not @event.creator_id.nil?
   end
 
   test "association invitation exist" do
-    assert_not @event.attendees.empty?
+    @user.save
+    @event.creator_id = @user.id
+    @event.save
+    assert_not @event.attendees.nil?
   end
 
-  # test "upcoming events included in Event.upcoming scope" do
-  #   assert_difference -> { Event.upcoming.count }, 1 do
-  #     @upcoming_event = Event.create!(title: 'Awesome party ya\'ll',
-  #                                            description: 'Gonna be lit',
-  #                                            date: '06-20-2020',
-  #                                            place: 'Kenya',
-  #                                            creator_id: @user.id)
-  #   end
-  #   assert_includes Event.upcoming, @upcoming_event
-  # end
 end
